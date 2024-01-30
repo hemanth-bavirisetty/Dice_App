8e019e388fa5109e15812dfb202f28a836998e7c
[Hemanth Bavirisetty] <[manikantahemanth5177@gmail.com]>
2024-01-30 15:14:09 +0530

    Created a widget class

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: SweepGradient(
-              colors: [
-                Colors.blueAccent,
-                Colors.lightBlue,
-                Colors.blueGrey,
-                Colors.white,
-                Colors.amber,
-                Colors.greenAccent,
-                Colors.lightGreen,
-                Colors.blueAccent
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hey there, Welocme!!',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  ),
-            ),
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: SweepGradient(
+          colors: [
+            Colors.blueAccent,
+            Colors.lightBlue,
+            Colors.blueGrey,
+            Colors.white,
+            Colors.amber,
+            Colors.greenAccent,
+            Colors.lightGreen,
+            Colors.blueAccent
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hey there, Welocme!!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
